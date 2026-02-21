224fe639729aaf21a56a8fb2fc8e2676e8755a42
Rija Lalaina <rijalaraz@gmail.com>
2026-02-21 16:17:25 +0300

    Create product API documentation

--- src/Catalogue/Presentation/Http/Controller/ProductController.php
+++ src/Catalogue/Presentation/Http/Controller/ProductController.php
@@ -7,10 +7,12 @@ use App\Catalogue\Application\Command\Handler\CreateProductCommandHandler;
 use App\Catalogue\Application\Query\GetProductsQuery;
 use App\Catalogue\Application\Query\Handler\GetProductsQueryHandler;
 use App\SharedKernel\Http\ResponseEnvelope;
+use Nelmio\ApiDocBundle\Attribute\Model;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Uid\Uuid;
+use OpenApi\Attributes as OA;
 
 #[Route('/api/products')]
 class ProductController
@@ -24,10 +26,25 @@ class ProductController
     }
 
     #[Route('', name: 'products.create', methods: ['POST'])]
+    #[OA\RequestBody(
+        description: 'Product data',
+        required: true,
+        content: new OA\JsonContent(ref: new Model(type: CreateProductCommand::class, groups: ['product:create']))
+    )]
+    #[OA\Response(
+        response: JsonResponse::HTTP_CREATED,
+        description: 'Product created successfully',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(property: 'data', ref: new Model(type: CreateProductCommand::class, groups: ['product:detail'])),
+            ]
+        )
+    )]
     public function create(CreateProductCommandHandler $handler, Request $request): JsonResponse
     {
+        $data = json_decode($request->getContent(), true);
         $command = new CreateProductCommand(
-            Uuid::v7()->toString(), $request->get('name', ''), (int)$request->get('price', 0), (int)$request->get('on_hand', 0)
+            Uuid::v7()->toString(), $data['name'] ?? '', (int)($data['price'] ?? 0), (int)($data['onHand'] ?? 0)
         );
         $product = $handler($command);
         $envelope = ResponseEnvelope::success(['id' => $product->getId()], JsonResponse::HTTP_CREATED);
